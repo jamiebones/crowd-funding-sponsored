d835842809493c19551f5569f6b3a37dc02a5f79
jamiebones <jamiebones2000@yahoo.co.uk>
2025-02-09 05:22:17 +0100

    change url

--- apps/frontend/src/app/components/Navbar.tsx
+++ apps/frontend/src/app/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
             <Link
               href="/explore"
               className={`px-3 py-2 rounded-md text-sm font-medium ${
-                pathname === "/explore"
+                pathname === "/search"
                   ? "bg-gray-900 text-white"
                   : "text-gray-600 hover:bg-gray-700 hover:text-white"
               }`}
