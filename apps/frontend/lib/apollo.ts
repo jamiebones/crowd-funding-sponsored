12b312e3feaab85ef7b32ff85d3ad0873e755a97
jamiebones <jamiebones2000@yahoo.co.uk>
2025-12-04 10:49:27 +0100

    added authorization headers

--- apps/frontend/lib/apollo.ts
+++ apps/frontend/lib/apollo.ts
@@ -2,6 +2,11 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+    headers: {
+        authorization: process.env.NEXT_PUBLIC_BEARER_TOKEN 
+            ? `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}` 
+            : '',
+    },
 });
 
 export const apolloClient = new ApolloClient({
